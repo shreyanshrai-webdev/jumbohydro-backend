bef338a2d777948ce6a1005d37fe68529cc5ae64
shreyansh <raishreyansh417@gmail.com>
2026-05-01 21:57:55 +0530

    fix: remove duplicate CORS, fix env URLs

--- server.js
+++ server.js
@@ -19,16 +19,6 @@ app.use(
   }),
 );
 
-// Fix for Safari Tracking Prevention
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") return res.sendStatus(200);
-  next();
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
